dd6f05b093fc6e78fdcf47674ebe01fe7bd8cc11
liangzeng <1405491181@qq.com>
2018-05-13 21:23:10 +0800

    change site

--- template/components/Footer.vue
+++ template/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer>
     <p>我的视频课程主页： <a target="_blank" href="https://edu.csdn.net/lecturer/585">https://edu.csdn.net/lecturer/585</a> </p>
-    <p>我的网站 <a target="_blank" href="http://forjs.org">http://forjs.org</a> 即将开通。</p>
+    <p>我的网站 <a target="_blank" href="http://bookjs.cn">http://bookjs.cn</a> 即将开通。</p>
   </footer>
 </template>
