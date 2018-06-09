57267a00a34d9b1f9f4536d400913e7fffe5724c
liangzeng <1405491181@qq.com>
2018-06-10 07:45:39 +0800

    delete footer info

--- template/components/Footer.vue
+++ template/components/Footer.vue
@@ -1,6 +1,4 @@
 <template>
   <footer>
-    <p>我的视频课程主页： <a target="_blank" href="https://edu.csdn.net/lecturer/585">https://edu.csdn.net/lecturer/585</a> </p>
-    <p>我的网站 <a target="_blank" href="http://bookjs.cn">http://bookjs.cn</a> 即将开通。</p>
   </footer>
 </template>
