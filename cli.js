c6621dfed1c9d5ad8f8788819cb37866dd55c87f
liangzeng <1405491181@qq.com>
2018-06-10 10:40:58 +0800

    test

--- cli.js
+++ cli.js
@@ -1,6 +1,20 @@
 #!/usr/bin/env node
-const projectName = process.argv.pop();
+const argv = process.argv;
+const isUpdate = argv.includes('-U');
+const projectName = argv.pop();
 const fs = require('fs-extra');
 const path = require("path");
+
+if(isUpdate){
+  
+}
+const {dependencies} = require("./template/package.json");
+
+(async ()=>{
+  let projectPath = path.resolve(projectName,'server');
+  if(await fs.pathExists(file)){
+
+  }
+})();
 
 fs.copy(path.join(__dirname,"template"), projectName, {filter:(src,dist)=>!/node_modules/.test(src)});
