2922cbb035e579f5f148caef5b289723f6d0b3ed
liangzeng <1405491181@qq.com>
2018-06-10 10:44:44 +0800

    test

--- cli1.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-const projectName = process.argv.pop();
-const fs = require('fs-extra');
-const path = require("path");
-
-fs.copy(path.join(__dirname,"template"), projectName);
